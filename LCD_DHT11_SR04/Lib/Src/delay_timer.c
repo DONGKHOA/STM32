232e268b4e1eb324ec96686a82fb5b5db5afc6ee
DONGKHOA <thanhkhoa2k3@gmail.com>
2023-03-09 20:10:30 +0700

    update

--- LCD_DHT11_SR04/Lib/Src/delay_timer.c
+++ LCD_DHT11_SR04/Lib/Src/delay_timer.c
@@ -10,16 +10,6 @@ void DELAY_Tim_Init(TIM_HandleTypeDef *htim)
     HAL_TIM_Base_Start(htim);
 }
 
-/**
- * "This function takes a pointer to a timer handle and a time in microseconds and sets the timer
- * counter to zero and then waits until the timer counter is greater than the time in microseconds."
- * 
- * The first line of the function sets the timer counter to zero. The second line is a while loop that
- * waits until the timer counter is greater than the time in microseconds
- * 
- * @param htim pointer to the timer handle
- * @param time the time in microseconds
- */
 void DELAY_Tim_Us(TIM_HandleTypeDef *htim, uint16_t time_us)
 {
 	HAL_TIM_Base_Start(htim);
@@ -28,13 +18,6 @@ void DELAY_Tim_Us(TIM_HandleTypeDef *htim, uint16_t time_us)
 	HAL_TIM_Base_Stop(htim);
 }
 
-/**
- * The function takes a pointer to a timer handle and a time in milliseconds. It sets the counter to
- * zero and then waits for the counter to reach the time in milliseconds.
- * 
- * @param htim pointer to the timer handle
- * @param time the time in milliseconds you want to delay
- */
 void DELAY_Tim_Ms(TIM_HandleTypeDef *htim, uint16_t time_ms)
 {
 	HAL_TIM_Base_Start(htim);
